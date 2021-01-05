2525e09d6afd42cb6fd23589fa14a7f990fa96ae
airmick <32387739+airmick@users.noreply.github.com>
2021-01-05 17:04:20 +0200

    frist comit

---
+++ app/src/main/java/dev/airmick/birthdaycard/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.airmick.birthdaycard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
